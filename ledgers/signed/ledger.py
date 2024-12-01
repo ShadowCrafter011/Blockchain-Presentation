87d32ac9be6ebef35b952ee7312dbaffab9af61c
Lukas <lkoe@bluewin.ch>
2024-12-01 22:42:43 +0000

    Added signed ledger

---
+++ ledgers/signed/ledger.py
@@ -0,0 +1,95 @@
+#!/bin/env python3
+
+
+from parser import SimpleLedgerArgparse
+from signer import Signer
+import os
+
+
+def main():
+    parser = SimpleLedgerArgparse()
+    signer = Signer()
+
+    if not os.path.isfile("ledger.txt"):
+        ledger = []
+    else:
+        with open("ledger.txt") as ledger_file:
+            ledger_lines = ledger_file.read().split("\n")
+            ledger = []
+            for line in ledger_lines:
+                if not (stripped_line := line.strip()):
+                    continue
+                name, amount, signature = stripped_line.split(", ")
+                if verify_transaction(signer, name, amount, signature):
+                    ledger.append({
+                        "name": name,
+                        "amount": float(amount),
+                        "signature": signature
+                    })
+                else:
+                    print(f"Invalid transaction was rejected: {name} payed {amount}")
+
+    match parser.command():
+        case "list":
+            total = 0
+            people_total = {}
+            for transaction in ledger:
+                person, amount = transaction["name"], transaction["amount"]
+                total += amount
+                if person in people_total:
+                    people_total[person] += amount
+                else:
+                    people_total[person] = amount
+
+            if len(people_total) == 0:
+                print("Ledger is empty")
+                return
+
+            per_person = total / len(people_total)
+
+            for person, payed in people_total.items():
+                people_total[person] = payed - per_person
+
+            people = sorted(people_total.items(), key=lambda x: x[1])
+
+            for person in people:
+                name, amount = person
+                if amount < 0:
+                    print(f"{name} owes {round(-amount, 2)}$")
+                elif amount > 0:
+                    print(f"{name} recieves {round(amount, 2)}$")
+                else:
+                    print(f"{name} owes and recieves nothing")
+
+
+        case "pay":
+            name, amount = parser.person(), parser.amount()
+            ledger.append({
+                "name": name,
+                "amount": amount,
+                "signature": signer.sign_message(f"{name}, {amount}", name)
+            })
+
+            print(f"Transaction successfull: {parser.person()} payed {parser.amount()}")
+            
+        case _:
+            raise ValueError(f"Command {parser.command()} was not recognized")
+
+    ledger_lines = []
+    for line in ledger:
+        name, amount, signature = line["name"], line["amount"], line["signature"]
+        ledger_lines.append(f"{name}, {amount}, {signature}")
+
+    with open("ledger.txt", "w") as ledger_file:
+        ledger_file.write("\n".join(ledger_lines))
+
+def verify_transaction(signer: Signer, name, amount, signature):
+    return signer.verify_signature(
+        f"{name}, {amount}",
+        name,
+        signature
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ ledgers/signed/signer.py
@@ -0,0 +1,79 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.exceptions import InvalidSignature
+import base64
+import os
+
+
+class Signer:
+    def __init__(self):
+        self.private_keys = {}
+
+        for pem_file in os.listdir("keys"):
+            if pem_file == ".keep":
+                continue
+
+            pem_path = os.path.join("keys", pem_file)
+
+            with open(pem_path, "rb") as key_file:
+                private_key = serialization.load_pem_private_key(
+                    key_file.read(),
+                    password=None,
+                )
+                self.private_keys[pem_file.removesuffix(".pem")] = private_key
+
+    def sign_message(self, message: str, name: str) -> str:
+        name = name.lower()
+
+        signature = self.__get_private_key(name).sign(
+            message.encode("utf-8"),
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return base64.b64encode(signature).decode("utf-8")
+
+    def verify_signature(self, message: str, name: str, signature: str):
+        name = name.lower()
+
+        public_key = self.__get_private_key(name).public_key()
+        try:
+            public_key.verify(
+                base64.b64decode(signature),
+                message.encode("utf-8"),
+                padding.PSS(
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH
+                ),
+                hashes.SHA256()
+            )
+        except:
+            return False
+        else:
+            return True
+
+    def __get_private_key(self, name: str):
+        if not name in self.private_keys:
+            print(f"Generate p-key for {name}")
+            private_key = self.__generate_private_key()
+            self.private_keys[name] = private_key
+            self.__save_private_key(private_key, name)
+            return private_key
+        return self.private_keys[name]
+
+    def __generate_private_key(self):
+        return rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048
+        )
+
+    def __save_private_key(self, private_key, name):
+        pem = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+        with open(f"keys/{name}.pem", "wb") as pem_file:
+            pem_file.write(pem)

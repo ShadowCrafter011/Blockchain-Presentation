951e9249ebc6af93067a4db1fb9569fbe1b5c0ab
Lukas <lkoe@bluewin.ch>
2024-12-05 20:00:42 +0000

    Added saldo.py script to print out saldo of all blockchain users

---
+++ ledgers/blockchain/saldo.py
@@ -0,0 +1,11 @@
+#!/bin/env python3
+
+from blockchain_saver import BlockChain
+
+def main():
+    saldo = BlockChain.load().saldo()
+    for name, amount in saldo.items():
+        print(f"{name} has {amount} DD")
+
+if __name__ == "__main__":
+    main()
